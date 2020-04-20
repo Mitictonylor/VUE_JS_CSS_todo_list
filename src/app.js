c6d35eb0702868c9210b491e07d324860ccd1b7b
Antonio Lorusso <mitictonylor@gmail.com>
2020-04-20 17:58:36 +0100

    add new todo  in the list"

--- src/app.js
+++ src/app.js
@@ -4,12 +4,17 @@ document.addEventListener("DOMContentLoaded", () =>{
 new Vue({
   el: "#app",
   data:{
-    todos: ["wash the car", "study", "tidy up"]
-        },
-  models:{
+      todos: ["wash the car", "study", "tidy up"],
+      newTodo: ""
+          },
+  methods:{
+      saveNewToDo: function(){
+                this.todos.push(this.newTodo);
+                this.newTodo = "";
+                }
 
   }
-})
+});
 
 
-})
+});
