f7be0e49e7b5548e8e1247d006476673893d9fc5
Antonio Lorusso <mitictonylor@gmail.com>
2020-04-20 17:37:19 +0100

    add the array in the browser view

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ import Vue from 'vue';
 document.addEventListener("DOMContentLoaded", () =>{
 new Vue({
   el: "#app",
+  data:{
+    todos: ["wash the car", "study", "tidy up"]
+        },
+  models:{
+
+  }
 })
 
 
